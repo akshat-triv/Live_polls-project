60d61397e4eaac0745cf9caa66e11184105bc9d7
Akshat Trivedi <akshattriv.iit@gmail.com>
2021-07-26 15:02:42 +0530

    chore: outsourced the database connection details to env file

--- DB.js
+++ DB.js
@@ -3,9 +3,9 @@ const AppError = require('./utils/appError');
 
 const connection = mysql.createConnection({
   host: '127.0.0.1',
-  user: 'root',
-  password: 'reymysterio619',
-  database: 'live_polling',
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DBNAME,
   insecureAuth: true,
 });
 
@@ -16,34 +16,34 @@ connection.connect(function (err) {
 
 exports.save = (obj, which) => {
   let poll = { ...obj };
-  connection.query(`INSERT INTO ${which} SET ?`, poll, function (
-    err,
-    result,
-    field
-  ) {
-    if (err) return new AppError(err.message, '400');
-    //console.log(result);
-  });
+  connection.query(
+    `INSERT INTO ${which} SET ?`,
+    poll,
+    function (err, result, field) {
+      if (err) return new AppError(err.message, '400');
+      //console.log(result);
+    }
+  );
 };
 
 exports.find = (id, which, callback) => {
   let data = [];
-  connection.query(`SELECT * FROM ${which} WHERE ?`, id, function (
-    err,
-    res,
-    field
-  ) {
-    if (err) return new AppError(err.message, '400');
-    //console.log(res);
-    for (var i of res) {
-      let tmp = { ...i };
-      data.push(tmp);
+  connection.query(
+    `SELECT * FROM ${which} WHERE ?`,
+    id,
+    function (err, res, field) {
+      if (err) return new AppError(err.message, '400');
+      //console.log(res);
+      for (var i of res) {
+        let tmp = { ...i };
+        data.push(tmp);
+      }
+      if (which === 'polls') {
+        if (data[0]) data = data[0];
+      }
+      callback(data);
     }
-    if (which === 'polls') {
-      if (data[0]) data = data[0];
-    }
-    callback(data);
-  });
+  );
 };
 
 exports.update = (obj, which) => {
@@ -53,13 +53,13 @@ exports.update = (obj, which) => {
 };
 
 exports.deleteAll = (obj, which) => {
-  const query = connection.query(`DELETE FROM ${which} WHERE ?`, obj, function (
-    err,
-    res,
-    field
-  ) {
-    if (err) return new AppError(err.message, '400');
-  });
+  const query = connection.query(
+    `DELETE FROM ${which} WHERE ?`,
+    obj,
+    function (err, res, field) {
+      if (err) return new AppError(err.message, '400');
+    }
+  );
 };
 
 exports.vote = (id, which) => {
@@ -70,11 +70,11 @@ exports.vote = (id, which) => {
     tmp = { poll_id: id };
   }
 
-  connection.query(`UPDATE ${which} SET votes=votes+1 WHERE ?`, tmp, function (
-    err,
-    res,
-    field
-  ) {
-    if (err) return new AppError(err.message, '400');
-  });
+  connection.query(
+    `UPDATE ${which} SET votes=votes+1 WHERE ?`,
+    tmp,
+    function (err, res, field) {
+      if (err) return new AppError(err.message, '400');
+    }
+  );
 };
